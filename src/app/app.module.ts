10520fbe91d43e62df14906126a89447ce20969c
ingrid <ingrijrm94@gmail.com>
2019-08-24 16:48:25 -0500

    Se agregó componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { MenuComponent } from './shared/menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PortafolioComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
