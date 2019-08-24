3fa6bc799f33b306fcd18d51269314dbfc973fc6
ingrid <ingrijrm94@gmail.com>
2019-08-24 17:47:13 -0500

    Se agregó componente about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PortafolioComponent,
     MenuComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
