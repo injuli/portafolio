894b05125f5ce3466cf904cb31a02f0c4524fb5b
ingrid <ingrijrm94@gmail.com>
2019-08-25 18:40:16 -0500

    Se creó servicio contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { PrincipalService } from './services/principal.service';
 import { AcercaService } from './services/acerca.service';
+import { ResumeService } from './services/resume.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor( public principalService: PrincipalService, public acercaService: AcercaService ) {
+  constructor( public principalService: PrincipalService, public acercaService: AcercaService, public resumeService: ResumeService ) {
 
   }
 }
