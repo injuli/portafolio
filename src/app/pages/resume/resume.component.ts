9abc915c6f867253b5a8f1135442cfe13aa0cf12
ingrid <ingrijrm94@gmail.com>
2019-08-25 18:29:07 -0500

    Se agregó contenido JSON al componente resume.html y url de firebase al servicio resume

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResumeService } from '../../services/resume.service';
 
 @Component({
   selector: 'app-resume',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public resumeService: ResumeService) { }
 
   ngOnInit() {
   }
--- src/app/services/resume.service.ts
+++ src/app/services/resume.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 
+import { Curriculo } from '../interfaces/curriculo.interface';
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ResumeService {
+  desc: Curriculo = {};
+  carga = true;
+  constructor( private http: HttpClient) {
+    this.cargarResume();
+   }
 
-  constructor() { }
+   private cargarResume() {
+    this.http.get('https://portafolio-html-59583.firebaseio.com/resume.json')
+    .subscribe( (resp: Curriculo) => {
+     this.carga = true;
+     this.desc = resp;
+   });
+   }
 }
