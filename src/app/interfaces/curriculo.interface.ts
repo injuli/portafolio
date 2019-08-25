dbfd2cadc7fde626a4a5f0088bd4af22bb788872
ingrid <ingrijrm94@gmail.com>
2019-08-25 17:58:50 -0500

    Se creo servicio resume e interface curriculo

---
+++ src/app/interfaces/curriculo.interface.ts
@@ -0,0 +1,19 @@
+export interface Curriculo {
+    subtitulo?: string;
+    subitiulo2?: string;
+    subitiulo3?: string;
+    subitiulo4?: string;
+    subitiulo5?: string;
+    subtituloInfo?: string;
+    subtituloInfo2?: string;
+    subtituloInfo3?: string;
+    subtituloInfo4?: string;
+    subtituloInfo5?: string;
+    titulo?: string;
+    titulo2?: string;
+    year?: string;
+    year2?: string;
+    year3?: string;
+    year4?: string;
+    year5?: string;
+}
