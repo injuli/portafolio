9b9126f1cc6834a080978e00cafa1c938a7a6ace
ingrid <ingrijrm94@gmail.com>
2019-08-25 21:04:54 -0500

    Se agregó JSON a componente contact.html y acomodo el componente portafolio.html

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(public contactService: ContactService) { }
 
   ngOnInit() {
   }
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+  desc = {};
+  carga = true;
 
-  constructor() { }
+  constructor( private http: HttpClient ) {
+    this.cargarContact();
+  }
+
+  private cargarContact() {
+    this.http.get('https://portafolio-html-59583.firebaseio.com/contact.json')
+    .subscribe( (resp) => {
+     this.carga = true;
+     this.desc = resp;
+   });
+  }
 }
