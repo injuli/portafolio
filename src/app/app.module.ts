ccc883deb4e015e328ee65446d2377234da64802
ingrid <ingrijrm94@gmail.com>
2019-08-26 21:35:43 -0500

    se agrego loading, falta acomodar que se visualice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { MenuComponent } from './shared/menu/menu.component';
+import { DesingComponent } from './pages/desing/desing.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ResumeComponent } from './pages/resume/resume.component';
 import { ContactComponent } from './pages/contact/contact.component';
+
+
 
 @NgModule({
   declarations: [
@@ -20,7 +23,9 @@ import { ContactComponent } from './pages/contact/contact.component';
     HomeComponent,
     AboutComponent,
     ResumeComponent,
-    ContactComponent
+    ContactComponent,
+    DesingComponent,
+
   ],
   imports: [
     BrowserModule,
