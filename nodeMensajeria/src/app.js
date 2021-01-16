b20786dcbc1c611bc0c70a6c90e66c8d63431851
Ingrid J <ingrijrm94@gmail.com>
2021-01-16 16:33:32 -0500

    update 3

--- nodeMensajeria/src/app.js
+++ nodeMensajeria/src/app.js
@@ -9,6 +9,12 @@ app.use(cors());
 
 const port = process.env.PORT || 8080;
 
+app.use(express.static('./dist/angular-heroku'));
+
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', { root: 'dist/angular-heroku/' });
+});
+
 app.post('/contact', function(req, res) {
     configMensaje(req.body);
     res.status(200).send();
