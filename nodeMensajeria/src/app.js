2611edea4999b6bd26e697fe9957d11c8f919ba9
Ingrid J <ingrijrm94@gmail.com>
2021-01-16 09:28:26 -0500

    port

--- nodeMensajeria/src/app.js
+++ nodeMensajeria/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 // Serve only the static files form the dist directory
 app.use(express.static(_dirname + '/dist'));
