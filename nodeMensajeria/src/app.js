fb5e4a01faa71e185588d5ca91ece48b91edfdab
Ingrid J <ingrijrm94@gmail.com>
2021-01-16 08:53:37 -0500

    docs update

--- nodeMensajeria/src/app.js
+++ nodeMensajeria/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const bodyParser = require('body-parser');
 const configMensaje = require('./configMensaje');
 const app = express();
@@ -7,7 +8,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
+// Serve only the static files form the dist directory
+app.use(express.static(_dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(_dirname + '/dist/index.html'));
+
+});
 
 app.post('/contact', function(req, res) {
     configMensaje(req.body);
