9e519885e817fd5e88f03f12dcdb2eea89293971
Ingrid J <ingrijrm94@gmail.com>
2021-01-16 16:03:36 -0500

    change port delete path

--- nodeMensajeria/src/app.js
+++ nodeMensajeria/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const bodyParser = require('body-parser');
 const configMensaje = require('./configMensaje');
 const app = express();
@@ -10,18 +9,10 @@ app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
 app.post('/contact', function(req, res) {
     configMensaje(req.body);
     res.status(200).send();
 });
 app.listen(3000, () => {
-    console.log('Servidor corriendo');
+    console.log(`Servidor corriendo ${port}`);
 });
