e83595f3dd0d05cb0e12d6153e664016fc5ad019
Ingrid J <ingrijrm94@gmail.com>
2021-01-16 17:58:15 -0500

    change localhost for windowlocate

--- src/app/services/mensaje.service.ts
+++ src/app/services/mensaje.service.ts
@@ -13,7 +13,7 @@ export class MensajeService {
   sendMessage(body){
     console.log('ingreso al sendmessage mensajeservice');
     console.log('mensae service', body);
-    return this._http.post('http://localhost:3000/contact', body);
+    return this._http.post('${window.location.hostname}:3000/contact', body);
     
   }  
 }
