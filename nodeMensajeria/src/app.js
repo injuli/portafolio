afff8d26194f13e5f255a893630b67240d51d841
Ingrid J <ingrijrm94@gmail.com>
2021-01-18 17:15:06 -0500

    change

--- nodeMensajeria/src/app.js
+++ nodeMensajeria/src/app.js
@@ -11,11 +11,6 @@ app.use(cors());
 let port = process.env.PORT || 8080;
 
 
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
 app.use(express.static(__dirname + '/dist/portafolio'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/portafolio/index.html'));
