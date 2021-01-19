a94b3583c99fcb5209f4f6cd36577dc5019b3739
Ingrid J <ingrijrm94@gmail.com>
2021-01-19 04:01:13 -0500

    update app port

--- nodeMensajeria/src/app.js
+++ nodeMensajeria/src/app.js
@@ -4,11 +4,11 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const configMensaje = require('./configMensaje');
 const app = express();
+const app2 = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-//Install express server
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/portafolio'));
@@ -18,17 +18,15 @@ app.get('/*', (req, res) =>
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
 
-
+const port = process.env.PORT || 8080;
 
 app.post('/contact', function(req, res) {
     configMensaje(req.body);
     res.status(200).send();
 });
 
-
-/*
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log(`Servidor corriendo ${port}`);
-});*/
+});
