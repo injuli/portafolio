09091d7c7e5e6a7a78f60fee1428e2df8b37041f
Ingrid <ingrijrm94@gmail.com>
2021-04-20 15:49:39 -0500

    Se acomodo menu y particulas falta acomodar z-index del home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,105 @@
 import { Component, OnInit } from '@angular/core';
 import { PrincipalService } from '../../services/principal.service';
-
+import { Container, Main } from 'tsparticles';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
+export class HomeComponent  {
+  id = 'tsparticles';
   constructor( public principalService: PrincipalService ) { }
 
-  ngOnInit() {
-  }
+  particlesOptions = {
+    background: {
+        color: {
+            value: 'none'
+        }
+    },
+    fpsLimit: 60,
+    interactivity: {
+        detectsOn: 'canvas',
+        events: {
+            onClick: {
+                enable: true,
+                mode: 'push'
+            },
+            onHover: {
+                enable: true,
+                mode: 'repulse'
+            },
+            resize: true
+        },
+        modes: {
+            bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40
+            },
+            push: {
+                quantity: 4
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4
+            }
+        }
+    },
+    position: {
+        value: 'fixed'
+    },
+    particles: {
+        color: {
+            value: '#ffffff'
+        },
+        links: {
+            color: '#ffffff',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1
+        },
+        collisions: {
+            enable: true
+        },
+        move: {
+            direction: 'none',
+            enable: true,
+            outMode: 'bounce',
+            random: false,
+            speed: 6,
+            straight: false
+        },
+        number: {
+            density: {
+                enable: true,
+                value_area: 800
+            },
+            value: 80
+        },
+        opacity: {
+            value: 0.5
+        },
+        shape: {
+            type: 'circle'
+        },
+        size: {
+            random: true,
+            value: 5
+        }
+    },
+    detectRetina: true
+};
+
+particlesLoaded(container: Container): void {
+    console.log(container);
+}
+
+particlesInit(main: Main): void {
+    console.log(main);
+
+    // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
+}
 
 }
